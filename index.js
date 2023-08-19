b0b3550c4ad26c6e95b61415173b62d673d6aed3
Gertkhk <gert.sarv@khk.ee>
2023-08-19 06:17:23 +0300

    Refactored code dublicate with Promice for data fetching

--- index.js
+++ index.js
@@ -67,26 +67,6 @@ app.post('/', (req,res) => {
         })
 })
 
-
-// app.post('/', (req, res) => {
-//     let city = req.body.cityname
-//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
-//     .then((response) => {
-//         return response.json()
-//     })
-//     .then((data) => {
-//         let description = data.weather[0].description
-//         let city = data.name
-//         let temp = Math.round(parseFloat(data.main.temp)-273.15)
-//         res.render('index', {
-//             description: description,
-//             city: city,
-//             temp: temp
-//         })
-//     })
-// })
-
-
 app.listen(3000, ()=> {
     console.log('Server has been started! http://localhost:3000')
 });
