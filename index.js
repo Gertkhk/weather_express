25dd98ffe82419e98be8a13eddffa4445012ecf2
Gertkhk <gert.sarv@khk.ee>
2023-08-19 05:57:57 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.get('/', (req,res) => {
         })
 })
 
-app.post('/', function (req,res){
+app.post('/', (req,res) => {
     let city = req.body.cityname
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
     getWeatherDataPromise(url)
@@ -66,6 +66,26 @@ app.post('/', function (req,res){
             res.render('index', data)
         })
 })
+
+
+// app.post('/', (req, res) => {
+//     let city = req.body.cityname
+//     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then((data) => {
+//         let description = data.weather[0].description
+//         let city = data.name
+//         let temp = Math.round(parseFloat(data.main.temp)-273.15)
+//         res.render('index', {
+//             description: description,
+//             city: city,
+//             temp: temp
+//         })
+//     })
+// })
+
 
 app.listen(3000, ()=> {
     console.log('Server has been started! http://localhost:3000')
